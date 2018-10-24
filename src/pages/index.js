cdd55d1568cda49e92396df0fa875bd560954793
Tranced <terhyang@gmail.com>
2018-10-23 19:47:10 -0700

    Personalizing this gatsby site

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,20 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi I'm Terence</h1>
+    <p>I'm a high school computer science/engineering teacher!</p>
+    <p>Just trying to find my first SWE job. 
+       Nowadays, when I'm not teaching, I mainly work in React/ES6,
+       Node.js, and Python.
+
+       In school, I learned about Java/C/C++, and
+       got good in Python. 
+
+       I've been teaching CS fundamentals for
+       a really long time and have made tons of short
+       workshops.
+
+       Can't wait to attend the JAMstack hackathon!</p>
   </Layout>
 )
 
